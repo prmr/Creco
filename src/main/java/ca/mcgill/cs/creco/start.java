98ea745b073771e7d92b6cbd3109cee49ace62cc
nishanth1991 <nishanthg1991@gmail.com>
2014-02-13 17:53:53 -0500

    Thrid time

--- src/main/java/ca/mcgill/cs/creco/start.java
+++ src/main/java/ca/mcgill/cs/creco/start.java
@@ -23,7 +23,7 @@ import ca.mcgill.cs.creco.util.DataPath;
 public class start {
 
     public static void main1(String[] args) throws IOException {
-     //  System.out.println("Hey there");
+       System.out.println("Hey there");
        URL url;
        String path = DataPath.get();
        System.out.println(path);
