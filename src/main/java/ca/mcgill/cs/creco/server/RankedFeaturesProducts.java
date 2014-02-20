b84bd9eb452127ca02fd6341fa86e794e468fc01
Edward Newell <edward.newell@gmail.com>
2014-02-20 17:17:28 -0500

    add getRankeFeaturesProducts to SearchService

--- src/main/java/ca/mcgill/cs/creco/server/RankedFeaturesProducts.java
+++ src/main/java/ca/mcgill/cs/creco/server/RankedFeaturesProducts.java
@@ -1,16 +1,20 @@
 package ca.mcgill.cs.creco.server;
 
-import ca.mcgill.cs.creco.web.model.search.ProductSearchResult;
+import java.util.List;
 
+import ca.mcgill.cs.creco.web.model.search.ProductSearchResult;
+import ca.mcgill.cs.creco.logic.model.ScoredAttribute;
 
 
 public class RankedFeaturesProducts {
-	private final List<ScoredAttribute> ratingList;
-	private final List<ScoredAttribute> specList;
-	private final ProductSearchResult luProdList;
+	private final List<ScoredAttribute> aRatingList;
+	private final List<ScoredAttribute> aSpecList;
+	private final ProductSearchResult aProductSearchResult;
 	
-	public RankedFeaturesProducts(list<ScoredAttribute> pRatingList, List<ScoredAttribute> specList)
+	public RankedFeaturesProducts(List<ScoredAttribute> pRatingList, List<ScoredAttribute> pSpecList, ProductSearchResult pProductSearchResult)
 	{
-		
+		this.aSpecList = pSpecList;
+		this.aRatingList = pRatingList;
+		this.aProductSearchResult = pProductSearchResult;
 	}
 }
