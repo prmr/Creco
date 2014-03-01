5739b7b0ff48c5f7ad0a2d1af170e1f389a0b6a1
prmr <prmr@gmx.com>
2014-03-01 11:48:16 -0500

    Issue #22 added license blocks.

--- src/main/java/ca/mcgill/cs/creco/data/stubs/AttributeStub.java
+++ src/main/java/ca/mcgill/cs/creco/data/stubs/AttributeStub.java
@@ -1,13 +1,31 @@
+/**
+ * Copyright 2014 McGill University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package ca.mcgill.cs.creco.data.stubs;
 
 /**
  * This class is designed to capture data from the CR database for attributes (ratings and specs) which are 
  * properties of products.  It is a superclass for RatingStub and SpecStub.
  * 
+ * Reflectively loaded by the GSON library, do not change the field names.
+ * 
  * @author enewe101
  *
  */
-public class AttributeStub {
+public class AttributeStub 
+{
 	public String displayName;
 	public String description;
 	public String attributeId;
--- src/main/java/ca/mcgill/cs/creco/data/stubs/DownLevelStub.java
+++ src/main/java/ca/mcgill/cs/creco/data/stubs/DownLevelStub.java
@@ -1,8 +1,25 @@
+/**
+ * Copyright 2014 McGill University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package ca.mcgill.cs.creco.data.stubs;
 
-import ca.mcgill.cs.creco.data.Category;
-
-public class DownLevelStub {
+/**
+ * Reflectively loaded by the GSON library, do not change the field names.
+ */
+public class DownLevelStub 
+{
 	public CategoryStub[] supercategory;
 	public CategoryStub[] category;
 	public CategoryStub[] subfranchise;
