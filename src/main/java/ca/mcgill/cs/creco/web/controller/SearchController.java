41a9699323619e4a85f33ca2fc331ce2be540972
nishanth1991 <nishanthg1991@gmail.com>
2014-03-06 20:55:04 -0500

    Added the experiment auto-complete

--- src/main/java/ca/mcgill/cs/creco/web/controller/SearchController.java
+++ src/main/java/ca/mcgill/cs/creco/web/controller/SearchController.java
@@ -26,15 +26,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.google.gson.Gson;
 
 import ca.mcgill.cs.creco.data.Attribute;
 import ca.mcgill.cs.creco.data.Category;
+import ca.mcgill.cs.creco.data.IDataStore;
 import ca.mcgill.cs.creco.logic.AttributeValue;
 import ca.mcgill.cs.creco.logic.ScoredAttribute;
 import ca.mcgill.cs.creco.logic.search.ScoredProduct;
@@ -83,6 +86,10 @@ public class SearchController
 
 	@Autowired
 	private FeatureVO userSpecFeatures;
+	
+	@Autowired
+	private IDataStore aDataStore;
+	
 	
 	
 	@ModelAttribute("mainQuery")
@@ -151,7 +158,165 @@ public class SearchController
 		
 	}	
 	
+
+	// Levenshtein Distance used to calculate distance between two strings. 
+	public int LevenshteinDistance (String s0, String s1) {
+		int len0 = s0.length()+1;
+		int len1 = s1.length()+1;
+	 
+		// the array of distances
+		int[] cost = new int[len0];
+		int[] newcost = new int[len0];
+	 
+		// initial cost of skipping prefix in String s0
+		for(int i=0;i<len0;i++) cost[i]=i;
+	 
+		// dynamicaly computing the array of distances
+	 
+		// transformation cost for each letter in s1
+		for(int j=1;j<len1;j++) {
+	 
+			// initial cost of skipping prefix in String s1
+			newcost[0]=j-1;
+	 
+			// transformation cost for each letter in s0
+			for(int i=1;i<len0;i++) {
+	 
+				// matching current letters in both strings
+				int match = (s0.charAt(i-1)==s1.charAt(j-1))?0:1;
+	 
+				// computing cost for each transformation
+				int cost_replace = cost[i-1]+match;
+				int cost_insert  = cost[i]+1;
+				int cost_delete  = newcost[i-1]+1;
+	 
+				// keep minimum cost
+				newcost[i] = Math.min(Math.min(cost_insert, cost_delete),cost_replace );
+			}
+	 
+			// swap cost/newcost arrays
+			int[] swap=cost; cost=newcost; newcost=swap;
+		}
+	 
+		// the distance is the cost for transforming all letters in both strings
+		return cost[len0-1];
+	}
 	
+	
+	
+	public String returnfournearest(String abcd)
+	{
+		 String ajax_return=new String();
+		   int min1=200;
+		   int min2=200;
+		   int min3=200;
+		   int min4=200;
+		   Category min1_s=null;
+		   Category min2_s=null;
+		   Category min3_s=null;
+		   Category min4_s=null;
+		 
+		   for (Category category123 : aDataStore.getEquivalenceClasses()) 
+			{
+			   int value;
+			   if(category123.getName().toLowerCase().contains(abcd.toLowerCase()))
+				   value=0;
+			   else
+		  value= LevenshteinDistance(abcd.toLowerCase(),category123.getName().toLowerCase());
+		  if(value<=min1)
+		  {
+			 min4=min3;
+			 min3=min2;
+			 min2=min1;
+			 min1=value;
+			 min4_s=min3_s;
+			 min3_s=min2_s;
+			 min2_s=min1_s;
+			 min1_s=category123;
+		  }
+		  else if(value<=min2)
+		  {
+			  min4=min3;
+				 min3=min2;
+				 min2=value;
+				 min4_s=min3_s;
+				 min3_s=min2_s;
+				 min2_s=category123;
+		  }
+		  else if(value<=min3)
+		  {
+			  min4=min3;
+				 min3=value;
+				 min4_s=min3_s;
+				 min3_s=category123;
+		  }
+		  else if(value<=min4)
+		  {
+			  min4=value;
+			  min4_s=category123;
+			}
+			}
+		   ajax_return = "";
+
+		ajax_return=ajax_return.concat("<form action=\"/searchRankedFeaturesProducts\" method=\"POST\">");
+		ajax_return=ajax_return.concat("<input id=\"id\" name=\"id\" type=\"hidden\" value="+min1_s.getName()+"></input>");
+		   ajax_return=ajax_return.concat("<input id=\"id\" name=\"id\" type=\"hidden\" value="+min1_s.getName()+"></input>");
+		   ajax_return=ajax_return.concat("<input type=\"submit\" value=\""+min1_s.getName()+"\"></input>") ;
+		   ajax_return=ajax_return.concat("	<span class=\"badge\">"+min1_s.getCount()+"</span>");
+		   ajax_return=ajax_return.concat( "</form>");
+		ajax_return=ajax_return.concat("<form action=\"/searchRankedFeaturesProducts\" method=\"POST\">");
+		ajax_return=ajax_return.concat("<input id=\"id\" name=\"id\" type=\"hidden\" value="+min2_s.getName()+"></input>");
+		   ajax_return=ajax_return.concat("<input id=\"id\" name=\"id\" type=\"hidden\" value="+min2_s.getName()+"></input>");
+		   ajax_return=ajax_return.concat("<input type=\"submit\" value=\""+min2_s.getName()+"\"></input>") ;
+		   ajax_return=ajax_return.concat("	<span class=\"badge\">"+min2_s.getCount()+"</span>");
+		   ajax_return=ajax_return.concat( "</form>");
+		ajax_return=ajax_return.concat("<form action=\"/searchRankedFeaturesProducts\" method=\"POST\">");
+		ajax_return=ajax_return.concat("<input id=\"id\" name=\"id\" type=\"hidden\" value="+min3_s.getName()+"></input>");
+		   ajax_return=ajax_return.concat("<input id=\"id\" name=\"id\" type=\"hidden\" value="+min3_s.getName()+"></input>");
+		   ajax_return=ajax_return.concat("<input type=\"submit\" value=\""+min3_s.getName()+"\"></input>") ;
+		   ajax_return=ajax_return.concat("	<span class=\"badge\">"+min3_s.getCount()+"</span>");
+		   ajax_return=ajax_return.concat( "</form>");
+		ajax_return=ajax_return.concat("<form action=\"/searchRankedFeaturesProducts\" method=\"POST\">");
+		ajax_return=ajax_return.concat("<input id=\"id\" name=\"id\" type=\"hidden\" value="+min4_s.getName()+"></input>");
+		   ajax_return=ajax_return.concat("<input id=\"id\" name=\"id\" type=\"hidden\" value="+min4_s.getName()+"></input>");
+		   ajax_return=ajax_return.concat("<input type=\"submit\" value=\""+min4_s.getName()+"\"></input>") ;
+		   ajax_return=ajax_return.concat("	<span class=\"badge\">"+min4_s.getCount()+"</span>");
+		   ajax_return=ajax_return.concat( "</form>");
+		   return (ajax_return);  
+	}
+	
+	
+	
+	   @RequestMapping(value="/ajax", method=RequestMethod.POST )  
+ @ResponseBody  
+ public String createSmartphone(@RequestBody String typedString) {  
+		   List<Category> categoryList = searchService.searchCategories(typedString);		
+
+		   int count=0;
+		   String ajax_code=new String();
+			ArrayList<EqcVO> eqcs = new ArrayList<EqcVO>();		
+			for (Category cat: categoryList) {
+				count++;
+				EqcVO eqc = new EqcVO();
+				   ajax_code=ajax_code.concat("<form action=\"/searchRankedFeaturesProducts\" method=\"POST\">");
+				   String s = new String("<input id=\"id\" name=\"id\" type=\"hidden\" value="+cat.getName()+"></input>");
+				   s = s.concat("<input id=\"id\" name=\"id\" type=\"hidden\" value="+cat.getName()+"></input>");
+				   s = s.concat("<input type=\"submit\" value=\""+cat.getName()+"\"></input>" );
+				   s = s.concat("	<span class=\"badge\">"+cat.getCount()+"</span>");
+				   s = s.concat("</form>");
+				   ajax_code=ajax_code.concat(s);
+				eqc.setId(cat.getId());
+				eqc.setName(cat.getName());
+				eqc.setCount(cat.getCount());
+				eqcs.add(eqc);
+			}
+			eqcList.setEqcs(eqcs);
+			if(count==0)
+				return(returnfournearest(typedString));
+			else		
+	  return(ajax_code);
+ }  
+	   
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String init(Model model)
 	{
@@ -159,6 +324,15 @@ public class SearchController
 		 model.addAttribute("myForm", form);
 
 		return "/index";
+	}
+	
+	@RequestMapping(value = "/experiment", method = RequestMethod.GET)
+	public String init2(Model model)
+	{
+		 UserFeatureModel form = new UserFeatureModel();
+		 model.addAttribute("myForm", form);
+
+		return "/experiment";
 	}
 	
 	@RequestMapping(value = "/searchEqClass", method = RequestMethod.POST)
