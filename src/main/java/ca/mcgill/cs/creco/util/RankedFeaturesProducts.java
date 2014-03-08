e58d35a80c613817971e4aab15f8a0e81c9c5f04
nishanth1991 <nishanthg1991@gmail.com>
2014-03-07 19:55:38 -0500

    RankedFeaturesProducts added

---
+++ src/main/java/ca/mcgill/cs/creco/util/RankedFeaturesProducts.java
@@ -0,0 +1,86 @@
+package ca.mcgill.cs.creco.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ca.mcgill.cs.creco.data.*;
+import ca.mcgill.cs.creco.logic.AttributeValue;
+import ca.mcgill.cs.creco.logic.ScoredAttribute;
+import ca.mcgill.cs.creco.logic.search.ScoredProduct;
+
+
+public class RankedFeaturesProducts {
+	private static List<ScoredProduct> main_aProductSearchResult;
+
+	private static List<ScoredAttribute> aRatingList;
+
+	private static List<ScoredAttribute> aSpecList;
+
+	private static List<ScoredProduct> aProductSearchResult;
+	
+public RankedFeaturesProducts()
+{
+	
+}
+	
+	public RankedFeaturesProducts(List<ScoredAttribute> pRatingList, List<ScoredAttribute> pSpecList, List<ScoredProduct> pProductSearchResult)
+	{
+		RankedFeaturesProducts.main_aProductSearchResult=pProductSearchResult;
+		RankedFeaturesProducts.aProductSearchResult=pProductSearchResult;
+		RankedFeaturesProducts.aSpecList = pSpecList;
+		RankedFeaturesProducts.aRatingList = pRatingList;
+
+	}
+	
+	public List<ScoredProduct> FilterandReturn(List<ScoredAttribute> pSpecList)
+	{
+		List<ScoredProduct> new_set = new ArrayList<ScoredProduct>();
+		for(ScoredProduct p: RankedFeaturesProducts.main_aProductSearchResult)
+		{
+			int flag=0;
+			Product product= p.getProduct();
+			Iterable<Attribute> specs = product.getSpecs();
+			for(Attribute a:specs)
+			{
+				for(ScoredAttribute b:pSpecList)
+				{
+					if(a.getName().equals(b.getAttributeName()))
+					{
+						if((b.getAttributeMean().toString()).equals("NA")||(b.getAttributeMean().toString()).equals("Data not available"))
+						{
+							continue;
+						}
+						if(!(String.valueOf(a.getValue()).equals(b.getAttributeMean().toString())))
+								{
+									flag=1;
+								}
+					}
+				}
+
+			}
+			if(flag==0)
+			{
+				new_set.add(p);
+			}
+		}
+		RankedFeaturesProducts.aProductSearchResult = new_set;
+		return(RankedFeaturesProducts.aProductSearchResult);
+	}
+
+
+	public List<ScoredProduct> getaProductSearchResult()
+	{
+		return aProductSearchResult;
+	}
+	
+	public List<ScoredAttribute> getaRatingList()
+	{
+		return aRatingList;
+	}
+	
+	public List<ScoredAttribute> getaSpecList()
+	{
+		return aSpecList;
+	}
+	
+}
