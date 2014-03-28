8c220ffb3262a0b4f80fc907e1b424a9688a5027
Andrew Sutcliffe <sutcliffea@gmail.com>
2014-03-27 22:51:31 -0400

    reverted back

--- src/main/java/ca/mcgill/cs/creco/logic/ScoredAttribute.java
+++ src/main/java/ca/mcgill/cs/creco/logic/ScoredAttribute.java
@@ -13,6 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+/**
+ * TODO fix the Attribtue correlation
+ */
 package ca.mcgill.cs.creco.logic;
 
 import java.util.ArrayList;
