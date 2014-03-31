bf9d891ab2adcfc0b608c38a11b8d2e13c31fc2a
forgues <gabriel.forgues@gmail.com>
2014-03-31 12:11:22 -0400

    Added unit tests for ProductRanker

--- src/test/java/ca/mcgill/cs/creco/logic/TestProductRanker.java
+++ src/test/java/ca/mcgill/cs/creco/logic/TestProductRanker.java
@@ -2,6 +2,7 @@ package ca.mcgill.cs.creco.logic;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Test;
@@ -30,6 +31,10 @@ public class TestProductRanker {
 	private static String TOASTER_CATEGORY_ID = "28732";
 	private static int TOASTER_CATEGORY_NUM_PRODUCTS = 407;
 	
+	private static final String HUMIDIFIER_CATEGORY_ID = "32968";
+	private static final String HUMIDIFIER_OUTPUT_ID = "4556";
+	private static final String HUMIDIFIER_FULL_TANK_WEIGHT_ID = "6929";
+	
 	@Test
 	public void testRankingContainsAllProducts()
 	{    
@@ -37,6 +42,55 @@ public class TestProductRanker {
 		List<ScoredAttribute> scoredAttributes = aAttributeExtractor.getAttributesForCategory(category.getId());
 		    
 		List<Product> scoredProducts = aProductRanker.rankProducts(scoredAttributes, category.getProducts());
+		
 		assertEquals(TOASTER_CATEGORY_NUM_PRODUCTS, scoredProducts.size());
+	}
+	
+	@Test
+	public void testRankNumericMoreIsBetter()
+	{
+		Category category = aDataStore.getCategory(HUMIDIFIER_CATEGORY_ID); 
+		ScoredAttribute humidifierOutputAttribute = aAttributeExtractor.getScoredAttributeInCategory(HUMIDIFIER_CATEGORY_ID, HUMIDIFIER_OUTPUT_ID);
+		
+		List<ScoredAttribute> scoredAttributes = new ArrayList<ScoredAttribute>();
+		scoredAttributes.add(humidifierOutputAttribute);
+		
+		List<Product> scoredProducts = aProductRanker.rankProducts(scoredAttributes, category.getProducts());
+		
+		// Highest humidifier output should be 5.0/5.0
+		assertEquals(scoredProducts.get(0).getAttribute(HUMIDIFIER_OUTPUT_ID).getTypedValue().getNumeric(), 5.0, 0.00001);
+	}
+	
+	@Test
+	public void testRankNumericLessIsBetter()
+	{
+		Category category = aDataStore.getCategory(HUMIDIFIER_CATEGORY_ID); 
+		
+		ScoredAttribute humidifierWeightAttribute = aAttributeExtractor.getScoredAttributeInCategory(HUMIDIFIER_CATEGORY_ID, HUMIDIFIER_FULL_TANK_WEIGHT_ID);
+		
+		List<ScoredAttribute> scoredAttributes = new ArrayList<ScoredAttribute>();
+		scoredAttributes.add(humidifierWeightAttribute);
+		
+		List<Product> scoredProducts = aProductRanker.rankProducts(scoredAttributes, category.getProducts());
+		
+		// Lowest weight is 7.5
+		assertEquals(scoredProducts.get(0).getAttribute(HUMIDIFIER_FULL_TANK_WEIGHT_ID).getTypedValue().getNumeric(), 7.5, 0.0001);	
+	}
+	
+	@Test
+	public void testRankByTwoNumeric()
+	{
+		Category category = aDataStore.getCategory(HUMIDIFIER_CATEGORY_ID); 
+		
+		ScoredAttribute humidifierOutputAttribute = aAttributeExtractor.getScoredAttributeInCategory(HUMIDIFIER_CATEGORY_ID, HUMIDIFIER_OUTPUT_ID);
+		ScoredAttribute humidifierWeightAttribute = aAttributeExtractor.getScoredAttributeInCategory(HUMIDIFIER_CATEGORY_ID, HUMIDIFIER_FULL_TANK_WEIGHT_ID);
+		
+		List<ScoredAttribute> scoredAttributes = new ArrayList<ScoredAttribute>();
+		scoredAttributes.add(humidifierOutputAttribute);
+		scoredAttributes.add(humidifierWeightAttribute);
+		
+		List<Product> scoredProducts = aProductRanker.rankProducts(scoredAttributes, category.getProducts());
+		
+		assertEquals(scoredProducts.get(0).getName(), "Safety 1st 49292");
 	}
 }
