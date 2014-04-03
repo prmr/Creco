23a23e1448bca150e551648bce7042f19a53caaa
Andrew Sutcliffe <sutcliffea@gmail.com>
2014-04-03 17:33:38 -0400

    #71 Linked up TestScoredAttribute properly, I will write the tests over the weekend.

--- src/test/java/ca/mcgill/cs/creco/logic/TestScoredAttribute.java
+++ src/test/java/ca/mcgill/cs/creco/logic/TestScoredAttribute.java
@@ -7,13 +7,18 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import ca.mcgill.cs.creco.data.Attribute;
 import ca.mcgill.cs.creco.data.Category;
 import ca.mcgill.cs.creco.data.IDataStore;
 import ca.mcgill.cs.creco.data.Product;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:/META-INF/test-context.xml"})
 public class TestScoredAttribute {
 
 	@Autowired
@@ -29,8 +34,8 @@ public class TestScoredAttribute {
 	public void testSorting() throws NoSuchMethodException, SecurityException 
 	{
 		
-		Constructor<Product> c = Product.class.getDeclaredConstructor();
-		c.setAccessible(true);
+		//Constructor<Product> c = Product.class.getDeclaredConstructor();
+		//c.setAccessible(true);
 		//Product u = c.newInstance( ); 
 
 		
